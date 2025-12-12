85796754ea59338bd4715e587fe784beab84610b
Nayan Chaturvedi <123476375+Nayan-Chaturvedi@users.noreply.github.com>
2025-12-12 11:14:26 +0530

    Enhance CountTheNumberOfStringThatStartWithSpecificCharacter
    Added functionality to count numbers starting with '2'.

--- src/StreamQues/CountTheNumberOfStringThatStartWithSpecificCharacter.java
+++ src/StreamQues/CountTheNumberOfStringThatStartWithSpecificCharacter.java
@@ -11,3 +11,7 @@ public class CountTheNumberOfStringThatStartWithSpecificCharacter
 		System.out.println(count); //3
 	}
 }
+
+// Number start with 2
+List<Integer> al=Arrays.asList(21, 3, 23, 45, 65, 1, 69, 54);
+		al.stream().filter(num->num.toString().startsWith("2")).forEach(System.out::println);
